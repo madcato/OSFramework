aebdc304fa77af5419392bfaff4dcdca8c6e2c86
Daniel Vela <dani_vela@me.com>
2016-09-20 16:33:58 +0200

    initial commit

---
+++ OSFramework/OSFramework.h
@@ -0,0 +1,19 @@
+//
+//  OSFramework.h
+//  OSFramework
+//
+//  Created by Daniel Vela on 20/09/2016.
+//  Copyright © 2016 Daniel Vela. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OSFramework.
+FOUNDATION_EXPORT double OSFrameworkVersionNumber;
+
+//! Project version string for OSFramework.
+FOUNDATION_EXPORT const unsigned char OSFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OSFramework/PublicHeader.h>
+
+
---
+++ OSFramework/OSiCloudDataBackup.swift
@@ -0,0 +1,13 @@
+//
+//  OSiCloudDataBackup.swift
+//  OSFramework
+//
+//  Created by Daniel Vela on 20/09/2016.
+//  Copyright © 2016 Daniel Vela. All rights reserved.
+//
+
+import UIKit
+
+class OSiCloudDataBackup: NSObject {
+
+}
