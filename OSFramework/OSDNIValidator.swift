3711fa5c0c5a5878c7d4201c25e56949d3326ab2
Daniel Vela <dani_vela@me.com>
2016-09-22 23:00:09 +0200

    Added makeRange to String extension

--- OSFramework/OSDNIValidator.swift
+++ OSFramework/OSDNIValidator.swift
@@ -26,22 +26,17 @@ public class OSDNIValidator: NSObject {
         guard dni.characters.count == 9 else { return false }
 
         var buffer = dni.uppercased()
-        let f = buffer.startIndex
-        let t = buffer.index(buffer.startIndex, offsetBy: 1)
-        buffer = buffer.replacingOccurrences(of: "X", with: "0", options: .caseInsensitive, range: f..<t)
-        buffer = buffer.replacingOccurrences(of: "Y", with: "1", options: .caseInsensitive, range: f..<t)
-        buffer = buffer.replacingOccurrences(of: "Z", with: "2", options: .caseInsensitive, range: f..<t)
+        let range = buffer.makeRange(0..<1)
+        buffer = buffer.replacingOccurrences(of: "X", with: "0", options: .caseInsensitive, range: range)
+        buffer = buffer.replacingOccurrences(of: "Y", with: "1", options: .caseInsensitive, range: range)
+        buffer = buffer.replacingOccurrences(of: "Z", with: "2", options: .caseInsensitive, range: range)
 
         let number = buffer.substring(with: buffer.startIndex..<(buffer.index(buffer.endIndex, offsetBy: -1)))
         let baseNumber = Int(number)
         let letterMap = "TRWAGMYFPDXBNJZSQVHLCKET"
         let lettersIds = baseNumber! % 23
-        let from = letterMap.index(letterMap.startIndex, offsetBy: lettersIds)
-        let to = letterMap.index(letterMap.startIndex, offsetBy: lettersIds+1)
-        let expectedLetter = letterMap.substring(with: from..<to)
-        let from2 = buffer.index(buffer.startIndex, offsetBy: 8)
-        let to2 = buffer.index(buffer.startIndex, offsetBy: 9)
-        let providedLetter = buffer.substring(with: from2..<to2)
+        let expectedLetter = letterMap[lettersIds]
+        let providedLetter = buffer[8]
         return expectedLetter == providedLetter
     }
 }
---
+++ OSFramework/String+subscript.swift
@@ -0,0 +1,33 @@
+//
+//  String+subscript.swift
+//  happic-ios
+//
+//  Created by Daniel Vela on 29/04/16.
+//  Copyright © 2016 Daniel Vela. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    var length : Int {
+        return self.characters.count
+    }
+    
+    subscript(integerIndex: Int) -> Character {
+        let index = characters.index(startIndex, offsetBy: integerIndex)
+        return self[index]
+    }
+    
+    subscript(integerRange: Range<Int>) -> String {
+        let start = characters.index(startIndex, offsetBy: integerRange.lowerBound)
+        let end = characters.index(startIndex, offsetBy: integerRange.upperBound)
+        let range = start..<end
+        return self[range]
+    }
+
+    func makeRange(_ integerRange: Range<Int>) -> Range<String.CharacterView.Index> {
+        let start = characters.index(startIndex, offsetBy: integerRange.lowerBound)
+        let end = characters.index(startIndex, offsetBy: integerRange.upperBound)
+        return start..<end
+    }
+}
