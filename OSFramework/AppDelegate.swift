ea0aee9f762dcc2ba311dee5621a2ed133f51cf5
Daniel Vela <dani_vela@me.com>
2017-01-30 11:28:14 +0100

    Added helper for http capture

---
+++ OSFramework/AppDelegate.swift
@@ -0,0 +1,79 @@
+//
+//  AppDelegate.swift
+//  OSFramework
+//
+//  Created by Daniel Vela on 30/01/2017.
+//  Copyright © 2017 Daniel Vela. All rights reserved.
+//
+
+import Foundation
+
+
+// Sample code. Don't compile it
+
+
+@available(iOS 9.0, *)
+func application(_ application: UIApplication,
+                 open url: URL, options: [UIApplicationOpenURLOptionsKey: Any]) -> Bool {
+    if url.scheme == "happic" {
+        return handleHappicScheme(url)
+    }
+    return false
+}
+
+func application(_ application: UIApplication,
+                 open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+    if url.scheme == "happic" {
+        return handleHappicScheme(url)
+    }
+    
+    return false
+}
+
+func handleHappicScheme(_ url: URL) -> Bool {
+    
+    let pathComponents = url.pathComponents
+    
+    let firstComponent = pathComponents[1]
+    
+    if firstComponent == "share" {
+        if self.session.userIdentification.count == 0 {
+            // Login is not already made. Wait until made it
+            shareURL = url
+        } else {
+            askUserToAccept(url)
+        }
+        return true
+    }
+    
+    if firstComponent == "register" {
+        HPCMomentManager.makeDataDirt()
+        
+        let otp = pathComponents[3]
+        
+        let mail = UserDefaults.standard.string(forKey: HPCLoginViewController.kUserEmailID)
+        
+        let name = UserDefaults.standard.string(forKey: HPCLoginViewController.kUserNameID)
+        
+        doRegister(otp, name: name!, mail: mail!)
+        return true
+    }
+    
+    if url.host == "payment" {
+        let result = firstComponent
+        if result == "ok" {
+            // FIXME Reload picture
+            let center = NotificationCenter.default
+            center.post(name: Notification.Name(rawValue: "PaymentResultOk"), object: nil)
+        }
+        if result == "error" {
+            // Toast "Se ha producido un error en el pago"
+            showToast("Error en el pago", submsg: nil)
+        }
+        if result == "cancelled" {
+            // Do nothing
+        }
+    }
+    
+    return false
+}
