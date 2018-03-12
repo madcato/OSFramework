077c10ead71c713bbd9c1a757b71e3395fcde604
Dani Vela <dani_vela@me.com>
2018-03-12 09:57:54 +0100

    Cleaned code. Removed some old, unneeded classes and some classes that must not be included in a shared framework.

--- Sources/OSFramework/UIViewController+navigationItem.swift
+++ Sources/OSFramework/UIViewController+navigationItem.swift
@@ -6,7 +6,6 @@
 //  Copyright © 2017 Daniel Vela. All rights reserved.
 //
 
-
 extension UIViewController {
     func setRightBarButtonItem(_ item: UIBarButtonItem) {
         navigationController?.navigationBar.topItem?.rightBarButtonItem = item
